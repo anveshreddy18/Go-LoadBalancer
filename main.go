f2cb51b6328fc1642a579e9d1c095181e170ed98
Anvesh Reddy Pinnapureddy <anvesh.reddy.pinnapureddy@sap.com>
2024-02-04 16:36:12 +0530

    Add helper functions

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"sync"
+	"sync/atomic"
 )
 
 // Define Backend struct
@@ -20,9 +21,61 @@ type ServerList struct {
 	current  uint64
 }
 
+func (b *Backend) setAlive(flag bool) {
+	b.mux.Lock()
+	b.Alive = flag
+	b.mux.Unlock()
+}
+
+func (b *Backend) isAlive() (alive bool) {
+	b.mux.RLock()
+	alive = b.Alive
+	b.mux.RUnlock()
+	return alive
+}
+
+func (s *ServerList) MarkBackendStatus(url *url.URL, flag bool) {
+	for _, b := range s.backends {
+		if b.URL.String() == url.String() {
+			b.setAlive(flag)
+			return
+		}
+	}
+}
+
+func (s *ServerList) AddBackend(b *Backend) {
+	s.backends = append(s.backends, b)
+}
+
+// NextIndex returns the next cyclic index to the s.current.
+// The function exists to provide atomicity in the addition operation
+func (s *ServerList) NextIndex() int {
+	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
+}
+
+func (s *ServerList) GetNextPeer() *Backend {
+	nextInd := s.NextIndex()
+	// Now loop in a cyclic way to find the next backend service that is alive
+	for i := nextInd; i < (len(s.backends) + nextInd); i++ {
+		curInd := i % len(s.backends)
+		if s.backends[curInd].isAlive() {
+			// Store this index in the s.current & return this backend
+			atomic.StoreUint64(&s.current, uint64(curInd))
+			return s.backends[curInd]
+		}
+	}
+	return nil
+}
+
 func main() {
 
 	// Create a list of Backend services without the Alive flag
+	// url1, _ := url.Parse("http://localhost:1")
+	// url2, _ := url.Parse("http://localhost:2")
+	// url3, _ := url.Parse("http://localhost:3")
+	// url4, _ := url.Parse("http://localhost:4")
+
+	// backendList := []Backend{{URL: url1}}
 
 	// Create the serverList struct.
 
